8c0990f97dfb5aab6b46b6a74d8ad09f60d468e5
cpdmpara <leebadagyumin0129@gmail.com>
2025-11-19 09:30:14 +0900

    fix: 기존 이메일 등록 여부 응답 코드 수정

--- router/auth.py
+++ router/auth.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Response, Depends
 from fastapi.responses import JSONResponse
-from database import get_db
 from model.dto import PreRegisterDTO
 from service.auth import verify_email_format, check_email_register_status, send_auth_mail
+from database import get_db
 
 router = APIRouter(prefix="/auth", tags=["/auth"])
 
@@ -12,7 +12,7 @@ def post_preregister(data: PreRegisterDTO, db=Depends(get_db)):
         return JSONResponse({"code": "INVALID_EMAIL_FORMAT"}, status_code=400)
     
     if check_email_register_status(data.email, db):
-        return JSONResponse({"code": "ALREADY_REGISTERED"}, status_code=400)
+        return JSONResponse({"code": "ALREADY_REGISTERED_EMAIL"}, status_code=400)
     
     send_auth_mail(data.email)
 
