5b7cfea9c99a9fc1947cb8bbaab542e1d1e9b1d9
cpdmpara <leebadagyumin0129@gmail.com>
2026-01-07 08:29:59 +0900

    fix: 왼쪽 조회 로직 수정

--- repository/ThingRepository.py
+++ repository/ThingRepository.py
@@ -31,12 +31,9 @@ class ThingRepsitory:
     def get_list(self, prefix: int, page: int) -> list[Thing]:
         if page >= 0:
             statement = select(Thing).where(Thing.prefix >= prefix).order_by(asc(Thing.prefix), asc(Thing.quantity)).offset(page * 20).limit(20)
-            return self.db.execute(statement).scalars().all()
         else: 
-            statement = select(Thing).where(Thing.prefix < prefix).order_by(asc(Thing.prefix), desc(Thing.quantity)).offset(-(page + 1) * 20).limit(20)
-            result = list(self.db.execute(statement).scalars().all())
-            result.reverse()
-            return result
+            statement = select(Thing).where(Thing.prefix < prefix).order_by(desc(Thing.prefix), desc(Thing.quantity)).offset(-(page + 1) * 20).limit(20)
+        return self.db.execute(statement).scalars().all()
 
     def get(self, thingId: int) -> Thing | None:
         statement = select(Thing).where(Thing.thingId == thingId)
